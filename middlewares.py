8ce0ad71f7e544fc1acbb293f973e0f788b4c536
Azzot <azzot88@gmail.com>
2025-01-15 22:35:46 -0500

    get

--- middlewares.py
+++ middlewares.py
@@ -13,11 +13,11 @@ class RegisterCheck(BaseMiddleware):
         if event.text and event.text == '/start':
             return await handler(event, data)
         
-        # Get data manager from bot data
-        data_manager = data['bot'].get('data_manager')
-        user_data = data_manager.get_user(event.from_user.id)
-            
+        # Get data manager from dispatcher context
+        data_manager = data.get('data_manager')
+        
         # Check if user is registered
+        user_data = data_manager.get_user(event.from_user.id) if data_manager else None
         if not user_data or not user_data.get('is_registered'):
             await event.answer(
                 "Please register first using /start command"
